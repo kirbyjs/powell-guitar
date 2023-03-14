17aad48ae3b864db3a287c4ea8ce13fbefb97b02
Kirby Simmons <kirbyjs@icloud.com>
2023-03-13 20:43:24 -0500

    fix: define env variables for build

--- src/services/contentful.ts
+++ src/services/contentful.ts
@@ -3,13 +3,13 @@ import { GraphQLClient } from 'graphql-request';
 const {
   VITE_CONTENTFUL_GRAPHQL,
   VITE_CONTENTFUL_SPACE_ID,
-  VITE_CONTENTFUL_BEARER_TOKEN,
+  VITE_PREVIEW_TOKEN,
 } = import.meta.env;
 const endpoint = `${VITE_CONTENTFUL_GRAPHQL}${VITE_CONTENTFUL_SPACE_ID}`;
 
 export const gqlClient = new GraphQLClient(endpoint, {
   fetch,
   headers: {
-    authorization: `Bearer ${VITE_CONTENTFUL_BEARER_TOKEN}`,
+    authorization: `Bearer ${VITE_PREVIEW_TOKEN}`,
   },
 });
