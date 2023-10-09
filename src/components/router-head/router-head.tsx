c05368e2c5bb6ccb0646cef270b96ad9007f193c
Kirby Simmons <kirbyjs@icloud.com>
2023-10-08 22:54:36 -0500

    fix build errors

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -9,7 +9,7 @@ export const RouterHead = component$(() => {
     <>
       <title>{head.title}</title>
 
-      <link rel="canonical" href={loc.href} />
+      <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
